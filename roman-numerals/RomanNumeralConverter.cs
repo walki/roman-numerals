7c795d2da93e9e3d6ff46898a74291fb4fbfa32e
Roger Walker <roger.john.walker@gmail.com>
2022-04-15 18:57:20 -0400

    First test passing, 1 -> I

---
+++ roman-numerals/RomanNumeralConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace roman_numerals
+{
+    internal class RomanNumeralConverter
+    {
+        public RomanNumeralConverter()
+        {
+        }
+
+        internal string Convert(int v)
+        {
+            return "I";
+        }
+    }
+}
---
+++ roman-numerals/RomanNumeralsTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace roman_numerals
+{
+    [TestFixture]
+    class RomanNumeralsTest
+    {
+        [Test]
+        public void Nothing()
+        {
+            RomanNumeralConverter converter = new RomanNumeralConverter();
+            string one = converter.Convert(1);
+            Assert.That(one, Is.EqualTo("I"));
+        }
+
+
+    }
+}
